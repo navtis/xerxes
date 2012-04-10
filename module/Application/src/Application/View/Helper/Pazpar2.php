d633a547e7a4477058e7063060dd03ce210728f6
Graham Seaman <graham@theseamans.net>
2012-04-10 14:05:04 +0100

    Fixed pager

--- module/Application/src/Application/View/Helper/Pazpar2.php
+++ module/Application/src/Application/View/Helper/Pazpar2.php
@@ -112,6 +112,173 @@ class Pazpar2 extends Search
         }
     }
 
+	/**
+	 * Paging element
+	 * Overrides version in Helper/Search.php to add first/last
+     * options. Merge back in later? FIXME
+	 * @param int $total 		total # of hits for query
+	 * @param int $start 		First record on current page
+	 * @param int $max 			maximum number of results to show per page
+	 * 
+	 * @return DOMDocument formatted paging navigation
+	 */
+	
+	public function pager( $total, $start, $max )
+	{
+		if ( $total < 1 )
+		{
+			return null;
+		}
+		
+		$objXml = new \DOMDocument( );
+		$objXml->loadXML( "<pager />" );
+	
+		$base_record = 1; // starting record in any result set
+		$page_number = 1; // starting page number in any result set
+		$bolShowFirst = false; // show the first page when you get past page 10
+		$bolShowLast = false; // show the last page when it's past the numbered range shown
+		
+		if ( $start == 0 ) 
+		{
+			$start = 1;
+		}
+		
+		$current_page = (($start - 1) / $max) + 1; // calculates the current selected page
+		$bottom_range = $current_page - 4; // used to show a range of pages
+		$top_range = $current_page + 4; // used to show a range of pages
+		
+		$total_pages = ceil( $total / $max ); // calculates the total number of pages
+		
+		// for pages 1-8 show just 1-8 (or whatever records per page)
+		
+		if ( $bottom_range < 0 )
+		{
+			$bottom_range = 0;
+		}
+
+		if ( $bottom_range > 2 ) // as we already have the 'previous' link when page=2
+        {
+			$bolShowFirst = true;
+        }
+
+		if ( $current_page < 4 )
+		{
+			$top_range = 8;
+		} 
+		
+        if ( $total_pages > $top_range )
+        {
+            $bolShowLast = true;
+        }
+
+		// chop the top pages as we reach the end range
+		
+		if ( $top_range > $total_pages )
+		{
+			$top_range = $total_pages;
+		}
+		
+		// see if we even need a pager
+		
+		if ( $total > $max )
+		{
+			// create pages and links
+
+            if ( $bolShowFirst )
+            {
+			    $objPage = $objXml->createElement( "page", "|<" );
+						
+				$params = $this->currentParams();
+				$params["start"] = '1';
+						
+				$link = $this->request->url_for( $params );
+						
+				$objPage->setAttribute( "link", Parser::escapeXml( $link ) );
+				$objPage->setAttribute( "type", "first" );
+				$objXml->documentElement->appendChild( $objPage );
+			}
+
+			$previous = $start - $max;
+
+            if ( $start > $max )
+			{
+				$objPage = $objXml->createElement( "page", "<" ); // element to hold the text_results_previous label
+				
+				$params = $this->currentParams();
+				$params["start"] =  $previous;
+				
+				$link = $this->request->url_for( $params );
+				
+				$objPage->setAttribute( "link", Parser::escapeXml( $link ) );
+				//$objPage->setAttribute( "type", "previous" );
+				$objXml->documentElement->appendChild( $objPage );
+			}
+
+
+			while ( $base_record <= $total )
+			{
+				if ( $page_number >= $bottom_range && $page_number <= $top_range )
+				{
+					if ( $current_page == $page_number )
+					{
+						$objPage = $objXml->createElement( "page", $page_number );
+						$objPage->setAttribute( "here", "true" );
+						$objXml->documentElement->appendChild( $objPage );
+					} 
+					else
+					{
+						$objPage = $objXml->createElement( "page", $page_number );
+						
+						$params = $this->currentParams();
+						$params["start"] = $base_record;
+						
+						$link = $this->request->url_for( $params );
+						
+						$objPage->setAttribute( "link", Parser::escapeXml( $link ) );
+						$objXml->documentElement->appendChild( $objPage );
+					
+					}
+				}
+				
+				$page_number++;
+				$base_record += $max;
+			}
+			
+			$next = $start + $max;
+			
+			if ( $next <= $total )
+			{
+				$objPage = $objXml->createElement( "page", ">" ); // element to hold the text_results_next label
+				
+				$params = $this->currentParams();
+				$params["start"] =  $next;
+				
+				$link = $this->request->url_for( $params );
+				
+				$objPage->setAttribute( "link", Parser::escapeXml( $link ) );
+				//$objPage->setAttribute( "type", "next" );
+				$objXml->documentElement->appendChild( $objPage );
+			}
+
+            if ( $bolShowLast )
+            {
+			    $objPage = $objXml->createElement( "page", ">|" );
+						
+				$params = $this->currentParams();
+				$params["start"] = $total - $max + 1;
+						
+				$link = $this->request->url_for( $params );
+						
+				$objPage->setAttribute( "link", Parser::escapeXml( $link ) );
+				//$objPage->setAttribute( "type", "last" );
+				$objXml->documentElement->appendChild( $objPage );
+			}
+
+		}
+		
+		return $objXml;
+	}
+	
 }
 
 ?>
